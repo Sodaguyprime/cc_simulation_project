d82ad0b8b9637c619a5d471ab022b3f5156a9e64
Ammar Ahmed <152694573+Sodaguyprime@users.noreply.github.com>
2025-04-30 18:21:45 +0200

    Added sidebar to infogpage

--- Software/src/renderer/Pages/InfoPage.jsx
+++ Software/src/renderer/Pages/InfoPage.jsx
@@ -1,4 +1,44 @@
+
 import React from 'react';
-export default function InfoPage() {
-    return (<h1>This is the info page</h1>);
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import MyModel from '../components/MyModel';
+import Sidebar from '../components/sidebar';
+
+export default function App() {
+    return (
+        <div>
+
+            <div style={{
+                width: '200px',
+                position: 'fixed',
+                top: 0,
+                bottom: 0,
+                left: 0,
+                backgroundColor: '#f2f2f2',
+                borderRight: '1px solid #ccc',
+                zIndex: 10
+            }}>
+                <Sidebar />
+            </div>
+
+            s
+
+
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            borderStyle: "dashed",
+        } }>
+        <Canvas camera={{ position: [0, 2, 5] }}>s
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[0, 5, 5]} />
+            <MyModel scale={1.5} position={[0, 0, 0]} />
+            <OrbitControls />
+        </Canvas>
+        </div>
+        </div>
+    );
 }
